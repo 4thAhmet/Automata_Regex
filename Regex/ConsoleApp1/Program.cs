5ac9984b171f03e5f28fb78d9ef5fe75c67b757b
4thAhmet <ahmetaakkeci@gmail.com>
2022-10-25 22:46:56 +0300

    25-10-2022Son
    Fix Error

--- Regex/ConsoleApp1/Program.cs
+++ Regex/ConsoleApp1/Program.cs
@@ -1,29 +1,22 @@
 ﻿
-
 using System.Collections;
 
 internal class Program
 {
-    static int sayac = 0;
-    static int kontrol = 1;
     static ArrayList okunan = new ArrayList();
-    static ArrayList ifadeayristir = new ArrayList();
-    static Stack stack = new Stack();
-    static Stack stack1 = new Stack();
-    static string baskontrol = "";
-    static string sonkontrol = " ";
-    static string gelen;
+    static ArrayList list = new ArrayList();
+    static ArrayList okunacak = new ArrayList();
+    static int count = 0, say1 = 0, sayac = 0, kontrol = 1,basmi=0;
+    static string bas = " ", son = " ", orta = " ",gelen;
+
     private static void Main(string[] args)
     {
-
         Console.Write("Alfabe Giriniz: ");
         string L = Console.ReadLine();
         string[] alfabe = L.Split(',');
         ArrayList convertalfabe = new ArrayList();
         foreach (string s in alfabe)
-        {
             convertalfabe.Add(s);
-        }
 
         Console.Write("Düzenli İfadeyi Giriniz: ");
         string ifade = " ";
@@ -32,106 +25,68 @@ internal class Program
         Console.Write("Kaç Adet Göreceksiniz: ");
         int adet = Convert.ToInt32(Console.ReadLine());
         for (int i = 0; i < convertalfabe.Count; i++)
-        {
             okunan.Insert(i, convertalfabe[i]);
-        }
         AlfabeYap(convertalfabe, adet);
 
-
-        //EkranaBas(okunan, adet);
-         foreach (string s in okunacak)
-             Console.Write(s + ",");
-    }
-    static int ekranabassayac = 0;
-    static ArrayList okunacak = new ArrayList();
-    static void EkranaBas(ArrayList dizi, int adet)
-    {
-        if (ekranabassayac >= adet)
-            return;
-        foreach (string s in dizi)
-        {
-            //baskontrol = s.StartsWith('a').ToString();
-            //sonkontrol = s.Substring(s.Length - 1, 1);
-            if (s.StartsWith('a') == true && s.EndsWith('b') == true)
-            {
-                Console.WriteLine(s);
-                ekranabassayac++;
-                if (ekranabassayac >= adet)
-                    return;
-            }
-        }
-        /* if (ekranabassayac < adet)
-          {
-             AlfabeYap(okunacak, 50);
-             EkranaBas(okunan, adet);
-          }*/
-
+        foreach (string s in okunacak)
+            Console.Write(s + ",");
     }
 
-    // ( 40 )=41
-    //* = 42
-
-    static ArrayList list = new ArrayList();
-    static ArrayList Harf = new ArrayList();
-    static string bas = " ", son = " ";
     static void ifadeoku(string ifade)
     {
         char karakter;
-        string aktar;
         int kontrol = 0;
         for (int i = 0; i < ifade.Length; i++)
         {
             karakter = char.Parse(ifade.Substring(i, 1));
             if ((int)karakter >= 97 && (int)karakter <= 122 && kontrol == 0)
             {
-                if (i == 0)
-                    list.Add(karakter + "1");
-                else if (i == ifade.Length - 1)
-                    list.Add(karakter + "2");
-                else
-                    list.Add(karakter);
-                Console.Write(karakter.ToString() + " ");
-            }
-            else if ((int)karakter == 40 || kontrol == 1)
-            {
-                kontrol = 1;
-                Harf.Add(karakter);
-                if (karakter == ')')
+                if (i == 0 && ifade.Substring(1, 1) != "*")
                 {
-                    kontrol = 0;
+                    list.Add(karakter + "1"); //ab*b
+                    basmi = 1;
+                }              
+                else if (i == 0 && ifade.Substring(1, 1) == "*")
+                    list.Add(karakter + "*");
+                else if (i == ifade.Length - 1)
+                    list.Add(karakter + "/");
+                else
+                {
+                    if (ifade.Substring(i + 1, 1) == "*")
+                        list.Add(karakter + "*");
                 }
+                //Console.Write(karakter.ToString() + " ");
             }
         }
         string deger;
-
         for (int i = 0; i < list.Count; i++)
         {
             deger = list[i].ToString();
             if (deger.Substring(deger.Length - 1, 1) == "1")
+                bas = deger.Substring(0, 1);
+            else if (deger.Substring(deger.Length - 1, 1) == "*" && basmi == 0)
             {
                 bas = deger.Substring(0, 1);
+                orta = deger.Substring(0, 1);
             }
-            else if (deger.Substring(deger.Length - 1, 1) == "2")
-            {
+            else if (deger.Substring(deger.Length - 1, 1) == "/")
                 son = deger.Substring(0, 1);
-            }
-
+            else if (deger.Substring(deger.Length - 1, 1) == "*")
+                orta = deger.Substring(i - 1, 1);
         }
-        Console.WriteLine("bas: " + bas);
-        Console.WriteLine("son: " + son);
+        Console.Write("Baş Karakter: " + bas);
+        Console.Write("\tSon Karakter: " + son);
+        Console.Write("\tOrta Karakter: " + orta);
     }
 
-
-
-    static int count = 0;
-    static int say1 = 0;
     static void AlfabeYap(ArrayList alfabe, int adet)
     {
+        bool bak = false;
         count = alfabe.Count;
         if (say1 >= adet || count == 0) return;
         for (int i = 0; i < count; i++)
         {
-            for (int j = i; j < count; j++)
+            for (int j = 0; j < count; j++)
             {
                 for (int k = 0; k < okunan.Count; k++)
                 {
@@ -147,18 +102,41 @@ internal class Program
                 {
                     gelen = alfabe[i].ToString() + alfabe[j].ToString();
                     okunan.Add(alfabe[i].ToString() + alfabe[j].ToString());
-                    sayac++;
-                    if (gelen.Substring(0,1) == "a" && gelen.Substring(gelen.Length-1 ,1) == "b")
+                    if (gelen.Substring(0, 1) == bas && gelen.Substring(gelen.Length - 1, 1) == son && bas != " " && son != " ")
                     {
-                        say1++;
-                        okunacak.Add(gelen);
+                        if (gelen.Length <= 2)
+                            bak = true;
+                        else
+                        {
+                            bak = false;
+                            for (int k = 1; k <= gelen.Length - 2; k++)
+                            {
+                                if (gelen.Substring(k, 1) == orta)
+                                    bak = true;
+                                else
+                                {
+                                    bak = false;
+                                    break;
+                                }
+                            }
+                        }
+                        if (bak == true)
+                        {
+                           // Console.WriteLine("oldu: " + gelen);
+                            okunacak.Add(gelen);
+                            sayac++;
+                            say1++;
+                        }
+                        // else
+                        //Console.WriteLine("olmadı: " + gelen);
                     }
                     kontrol = 0;
                 }
                 if (say1 >= adet) return;
-
             }
         }
         AlfabeYap(okunan, adet);
     }
 }
+
+
